3008921d83f0424a1f3840b7160ec36d78a6a33e
AnriBost <149770184+AnriBost@users.noreply.github.com>
2023-11-03 12:47:06 +0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+ricxvi1 = int(input("gtxovt sheiyvanot pirveli ricxvi:"))
+ricxvi2 = int(input("gtxovt sheiyvanot meore ricxvi:"))
+operacia = input("gtxovt sheiyvanot operacia : + , - , * , /:")
+
+if operacia == "+":
+    print(ricxvi1+ricxvi2)
+if operacia == "-":
+    print(ricxvi1-ricxvi2)
+if operacia == "*":
+    print(ricxvi1*ricxvi2)
+if operacia == "/":
+    print(ricxvi1/ricxvi2)
